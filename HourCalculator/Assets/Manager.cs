7fecfa7179bc572249d56f25933f013c89b1bb49
Kjip <kjipgamer@gmail.com>
2019-06-04 21:59:41 +0200

    Fixed input bug

--- HourCalculator/Assets/Manager.cs
+++ HourCalculator/Assets/Manager.cs
@@ -24,12 +24,15 @@ public class Manager : MonoBehaviour
     }
 
     public void CurrentTime() {
+        Debug.Log(DateTime.Now.TimeOfDay.ToString());
         string[] temp = DateTime.Now.TimeOfDay.ToString().Split(':');
+        temp[2] = temp[2].Substring(0, 2);
+        Debug.Log(hours + ":" + minutes + ":" + seconds);
         hours = int.Parse(temp[0]);
         minutes = int.Parse(temp[1]);
         seconds = int.Parse(temp[2]);
 
-        currenthr24.text = string.Format("{0:00}:{1:00}", hours, minutes);
+        currenthr24.text = string.Format("{0:00}:{1:00}{2:00}", hours, minutes, seconds);
 
         if (hours > 12)
         {
