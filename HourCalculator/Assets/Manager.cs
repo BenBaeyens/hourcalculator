474a3fb9ca19454ebb63db9fb704481b32ac863d
Kjip <kjipgamer@gmail.com>
2019-06-04 21:50:20 +0200

    Added hour calculator

--- HourCalculator/Assets/Manager.cs
+++ HourCalculator/Assets/Manager.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
 
 public class Manager : MonoBehaviour
 {
@@ -15,4 +17,36 @@ public class Manager : MonoBehaviour
     {
         
     }
+
+    public void CalculateTime(int t) {
+        string[] temp = DateTime.Now.TimeOfDay.ToString().Split(':');
+        hours = int.Parse(temp[0]);
+        minutes = int.Parse(temp[1]);
+
+        minutes += 14;
+        for (int i = 0; i < t; i++)
+        {
+
+            hours += 1;
+            minutes += 50;
+
+            if (minutes >= 70)
+            {
+                minutes -= 70;
+                hours++;
+            }
+            if (minutes >= 60)
+            {
+                minutes -= 60;
+                hours += 1;
+            }
+            if (hours >= 24)
+                hours -= 24;
+        }
+
+        time = string.Format("{0:00}:{1:00}", hours, minutes);
+
+    }
+
+
 }
